b5053ef8c8efd442644c069802ea0bc7053150f7
Emre Topcu <114030199+EmreTopcu04@users.noreply.github.com>
2024-12-05 00:16:32 +0300

    Update Calculating Logic

--- Indicators/indicator_calculator.py
+++ Indicators/indicator_calculator.py
@@ -10,19 +10,20 @@ class IndicatorCalculator:
         self.macd_signal = macd_signal
         self.adx_length = adx_length
 
+    def calculate_indicators(self, df):
+        # Calculate RSI using the initialized rsi_length
+        df['rsi'] = ta.rsi(df['Close'], length=self.rsi_length)
+        
+        # Calculate short and long term moving averages
+        df['short_term_ma'] = df['Close'].rolling(window=self.rsi_length).mean()
+        df['long_term_ma'] = df['Close'].rolling(window=2 * self.rsi_length).mean()
 
-    def calculate_indicators(self,df):
-        # Calculate RSI
-        df['rsi'] = ta.rsi(df['Close'], length=14)
-        df['short_term_ma'] = df['Close'].rolling(window=14).mean()
-        df['long_term_ma'] = df['Close'].rolling(window=2 * 14).mean()
-
-        # Calculate MACD Histogram
-        macd = ta.macd(df['Close'])
+        # Calculate MACD Histogram using initialized macd_fast, macd_slow, and macd_signal
+        macd = ta.macd(df['Close'], fast=self.macd_fast, slow=self.macd_slow, signal=self.macd_signal)
         df['macd'] = macd['MACDh_12_26_9']
 
-        # Calculate ADX and its components
-        adx = ta.adx(df['High'], df['Low'], df['Close'], length=14)
+        # Calculate ADX and its components using the initialized adx_length
+        adx = ta.adx(df['High'], df['Low'], df['Close'], length=self.adx_length)
         df['adx'] = adx['ADX_14']  # Average Directional Index
         df['di_plus'] = adx['DMP_14']  # Positive Directional Indicator
         df['di_minus'] = adx['DMN_14']  # Negative Directional Indicator
