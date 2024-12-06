65b9bb2ae342ae772893cfbac3e4ea09f47f4501
Mazlum Arcanlı <20180808055@ogr.akdeniz.edu.tr>
2024-12-06 21:51:52 +0300

    macd
    Updated the MACDStrategy to include the Stochastic Oscillator, enhancing buy and sell signals by requiring the Stochastic K line to be above 80 for buys and below 20 for sells, in addition to existing MACD, RSI, and ADX conditions.

--- Strategies/macd_strategy.py
+++ Strategies/macd_strategy.py
@@ -1,4 +1,5 @@
 from .strategy_interface import TradingStrategy
+import pandas_ta as ta
 
 class MACDStrategy(TradingStrategy):
     def __init__(self, stop_loss):
@@ -6,16 +7,17 @@ class MACDStrategy(TradingStrategy):
 
     def label_logic(self, row):
         """Labeling logic for MACD strategy."""
-        macd = row['macd']  # MACD Histogram value
+        macd = row['macd']
         adx = row['adx']
         rsi = row['rsi']
+        stochastic = row['stoch_k']  # Stochastic K line
 
-        # Buy Signal:
-        if macd > 0 and rsi > 60 and adx > 25:
+        # Enhanced Buy Signal:
+        if macd > 0 and rsi > 60 and adx > 25 and stochastic > 80:
             return "Buy"
 
-        # Sell Signal:
-        elif macd < 0 and rsi < 40 and adx > 25:
+        # Enhanced Sell Signal:
+        elif macd < 0 and rsi < 40 and adx > 25 and stochastic < 20:
             return "Sell"
 
         # Hold Signal:
@@ -23,4 +25,4 @@ class MACDStrategy(TradingStrategy):
 
     def feature_columns(self):
         """Define the feature columns for the MACD strategy."""
-        return ['macd', 'rsi', 'adx', 'Close', 'Volume']
+        return ['macd', 'rsi', 'adx', 'stoch_k', 'Close', 'Volume']
